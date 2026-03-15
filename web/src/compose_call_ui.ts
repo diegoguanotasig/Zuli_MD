f85ef2affc21cec05d562e65512f9d4ee72b0b7f
kousu <nick@kousu.ca>
2026-03-14 21:19:37 -0400

    integrations: Add Galène video call provider.
    It's open source, low resource, and easy to deploy: https://galene.org.
    
    No matter who adds the video call each topic is fixed to a specific call
    URL {GALENE_URL}/group/{channel}/{topic}. DMs, taking a cue from Zulip,
    deterministically use {GALENE_URL}/dm/{user1,user2,...,user3}-group.
    
    Zulip moderators/administrators/owners become Galène ops.
    In DMs everyone is a Galène op.
    
    Galène doesn't have a logo, so I made one as a placeholder.
    
    This was largely inspired by and my thanks go out to both of:
    
    - Fatih Sözüer <fatih@constructor.tech>
        in https://github.com/zulip/zulip/pull/35792
    - The Dance <thedance@amamu.io>
        in https://github.com/zulip/zulip/pull/36349
    
    Closes https://github.com/zulip/zulip/issues/38516.

--- web/src/compose_call_ui.ts
+++ web/src/compose_call_ui.ts
@@ -7,8 +7,12 @@ import * as compose_call from "./compose_call.ts";
 import {get_recipient_label} from "./compose_closed_ui.ts";
 import * as compose_ui from "./compose_ui.ts";
 import {$t, $t_html} from "./i18n.ts";
+import * as message_lists from "./message_lists.ts";
+import * as people from "./people.ts";
+import {unresolve_name} from "./resolved_topic.ts";
 import * as rows from "./rows.ts";
 import {current_user, realm} from "./state_data.ts";
+import * as stream_data from "./stream_data.ts";
 import * as ui_report from "./ui_report.ts";
 import * as util from "./util.ts";
 
@@ -159,6 +163,56 @@ export function generate_and_insert_audio_or_video_call_link(
                 xhr = channel.post({
                     url: "/json/calls/constructorgroups/create",
                     data: {},
+                    success(response) {
+                        if (xhr && compose_call.ignored_call_xhrs.has(xhr)) {
+                            return;
+                        }
+                        const data = call_response_schema.parse(response);
+                        insert_video_call_url(data.url, $target_textarea);
+                    },
+                    error(_xhr, status) {
+                        if (xhr && compose_call.ignored_call_xhrs.has(xhr)) {
+                            return;
+                        }
+                        if (status !== "abort") {
+                            ui_report.generic_embed_error(
+                                $t_html({defaultMessage: "Failed to create video call."}),
+                                2000,
+                            );
+                        }
+                    },
+                });
+                break;
+            }
+            case available_providers.galene?.id: {
+                const selected_message = message_lists?.current?.selected_message();
+                if (selected_message === undefined) {
+                    break;
+                }
+
+                let stream;
+                let topic;
+                if (selected_message?.is_stream) {
+                    // group chat
+                    stream = stream_data.get_sub_by_id(selected_message.stream_id)?.name;
+                    topic = unresolve_name(selected_message.topic);
+                } else {
+                    // DM
+                    // to_user_ids only includes the *other* users in the DM
+                    // make sure we have a consistent call topic no matter who started it
+                    const user_ids = people.user_ids_string_to_ids_array(
+                        selected_message.to_user_ids,
+                    );
+                    user_ids.push(current_user.user_id);
+                    topic = util.sorted_ids(user_ids).join(",");
+                }
+
+                xhr = channel.post({
+                    url: "/json/calls/galene/create",
+                    data: {
+                        channel: stream,
+                        topic,
+                    },
                     success(response) {
                         if (xhr && compose_call.ignored_call_xhrs.has(xhr)) {
                             return;
