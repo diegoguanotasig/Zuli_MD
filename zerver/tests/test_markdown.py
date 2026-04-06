zulip/zulip

lib: Switch convert_html_to_markdown to use markdownify.
markdownify is MIT-licensed and can be imported directly, unlike
html2text which is GPL-licensed and had to be run as a subprocess.

zerver/tests/test_markdown.py
@@ -3813,7 +3813,31 @@ class MarkdownErrorTests(ZulipTestCase):
 
 
 class TestHtmlToMarkdown(ZulipTestCase):
-    def test_unicode(self) -> None:
+    def test_html_entity_decoding(self) -> None:
         self.assertEqual(
             convert_html_to_markdown("a rose is not a ros&eacute;"), "a rose is not a rosé"
         )
+
+    def test_unordered_list_renders_as_bullets(self) -> None:
+        # Zulip requires "* item" (with space) to render as a bullet; html2text
+        # produced "*item" (no space), which rendered as a literal asterisk.
+        html = "<ul><li>foo</li><li>bar</li></ul>"
+        result = convert_html_to_markdown(html)
+        self.assertIn("* foo", result)
+        self.assertIn("* bar", result)
+
+    def test_atx_style_headings(self) -> None:
+        self.assertEqual(convert_html_to_markdown("<h1>Title</h1>"), "# Title")
+        self.assertEqual(convert_html_to_markdown("<h2>Sub</h2>"), "## Sub")
+
+    def test_img_tag_preserved_as_markdown_image(self) -> None:
+        html = '<img src="http://example.com/img.png" alt="photo">'
+        self.assertIn("![photo](http://example.com/img.png)", convert_html_to_markdown(html))
+
+    def test_anchor_tag_preserved_as_markdown_link(self) -> None:
+        html = '<a href="http://example.com">click here</a>'
+        self.assertEqual(convert_html_to_markdown(html), "[click here](http://example.com)")
+
+    def test_bold_and_italic(self) -> None:
+        self.assertEqual(convert_html_to_markdown("<strong>bold</strong>"), "**bold**")
+        self.assertEqual(convert_html_to_markdown("<em>italic</em>"), "*italic*")

diegoguanotasig/Zuli_MD

zerver/tests/test_markdown.py
@@ -3862,7 +3862,35 @@ class MarkdownErrorTests(ZulipTestCase):
 
 
 class TestHtmlToMarkdown(ZulipTestCase):
-    def test_unicode(self) -> None:
+    def test_html_entity_decoding(self) -> None:
         self.assertEqual(
             convert_html_to_markdown("a rose is not a ros&eacute;"), "a rose is not a rosé"
         )
+
+    def test_unordered_list_renders_as_bullets(self) -> None:
+        # Zulip requires "* item" (with space) to render as a bullet; html2text
+        # produced "*item" (no space), which rendered as a literal asterisk.
+        html = "<ul><li>foo</li><li>bar</li></ul>"
+        result = convert_html_to_markdown(html)
+        self.assertIn("* foo", result)
+        self.assertIn("* bar", result)
+
+    def test_atx_style_headings(self) -> None:
+        self.assertEqual(convert_html_to_markdown("<h1>Title</h1>"), "# Title")
+        self.assertEqual(convert_html_to_markdown("<h2>Sub</h2>"), "## Sub")
+
+    def test_img_tag_with_alt_preserved(self) -> None:
+        html = '<img src="http://example.com/img.png" alt="photo">'
+        self.assertIn("![photo](http://example.com/img.png)", convert_html_to_markdown(html))
+
+    def test_img_tag_without_alt_converted_to_link(self) -> None:
+        html = '<img src="http://foo.com/image.png?12345">'
+        self.assertEqual(convert_html_to_markdown(html), "[image.png](http://foo.com/image.png)")
+
+    def test_anchor_tag_preserved_as_markdown_link(self) -> None:
+        html = '<a href="http://example.com">click here</a>'
+        self.assertEqual(convert_html_to_markdown(html), "[click here](http://example.com)")
+
+    def test_bold_and_italic(self) -> None:
+        self.assertEqual(convert_html_to_markdown("<strong>bold</strong>"), "**bold**")
+        self.assertEqual(convert_html_to_markdown("<em>italic</em>"), "*italic*")